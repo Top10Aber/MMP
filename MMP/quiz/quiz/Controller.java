8220dd3bf3a01c87d09eeeb867ace42f79fffe78
Tommy <Tommy@10.87.1.31>
2018-03-27 16:06:00 +0100

    Fairly sure it's fixed now

--- MMP/quiz/quiz/Controller.java
+++ MMP/quiz/quiz/Controller.java
@@ -167,6 +167,7 @@ public class Controller extends Application {
 /*	public void prevQuestion() throws IOException {
 		questionNum --;
 		defaultNumOfAttempts = defaultNumOfAttempts - 1;
+		
 		}*/
 
 	
